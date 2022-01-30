0adc2d0888eedc6ee689158a5e89dd899333fc0d
entropy-sumit <sumitr600@gmail.com>
2022-01-30 17:52:47 +0530

    [add] sumit added product review management program

---
+++ ProductManagementReviewWithLinq/Management.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Linq;
+
+namespace ProductReviewMangementWithLinq
+{
+    public class Management
+    {
+    }
+}
---
+++ ProductManagementReviewWithLinq/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewMangementWithLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management problem Statement");
+
+        }
+    }
+}
