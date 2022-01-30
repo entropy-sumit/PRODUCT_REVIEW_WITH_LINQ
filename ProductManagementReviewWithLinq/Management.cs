f67792feb812308cf33bff5d9d7748cbc0a91bf7
entropy-sumit <sumitr600@gmail.com>
2022-01-30 23:33:39 +0530

    [add] sumit added product review management uc2 program

--- ProductManagementReviewWithLinq/Management.cs
+++ ProductManagementReviewWithLinq/Management.cs
@@ -8,5 +8,19 @@ namespace ProductReviewMangementWithLinq
 {
     public class Management
     {
+        public void TopRecordsList(List<ProductReview> review)
+        {
+            var recordData = (from productReviews in review
+                              orderby productReviews.Rating descending
+                              select productReviews).Take(3);
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductID: " + list.ProductID + " " + "UserId: " + list.UserID + " " + "Rating: " + list.Rating + " "
+                    + "Review: " + list.Review + " "+ "islike:" + list.isLike + " ");
+            }
+        }
+
+      
     }
 }
